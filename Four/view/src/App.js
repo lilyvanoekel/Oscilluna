d3c66e3c02545c3d97bf825a99e3a76519488f62
Lily <lilyvanoekel@gmail.com>
2024-08-25 09:18:49 -0400

    frontend noodling

--- Four/view/src/App.js
+++ Four/view/src/App.js
@@ -3,8 +3,21 @@ import DrawShapeToWaveform from "./DrawShapeToWaveform";
 
 function App({ patchConnection }) {
   return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <DrawShapeToWaveform patchConnection={patchConnection} />
+    <div style={{ display: "flex", width: "100%", height: "100%" }}>
+      <div
+        style={{
+          width: "120px",
+          background: "#222222",
+          padding: "20px",
+        }}
+      >
+        <button className="side-button">Wave 1</button>
+        <button className="side-button">Wave 2</button>
+        <button className="side-button">Wave Folding</button>
+      </div>
+      <div style={{ flex: 1 }}>
+        <DrawShapeToWaveform patchConnection={patchConnection} />
+      </div>
     </div>
   );
 }
