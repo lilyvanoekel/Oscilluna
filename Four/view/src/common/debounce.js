02312f000d84e6b4ac003948d13457047ef6c613
Lily van Oekel <lilyvanoekel@tactable.io>
2024-08-26 20:35:13 -0400

    delete old view

--- Four/view/src/common/debounce.js
+++
@@ -1,9 +0,0 @@
-export const debounce = (func, delay) => {
-  let timeoutId;
-  return (...args) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      func.apply(this, args);
-    }, delay);
-  };
-};
